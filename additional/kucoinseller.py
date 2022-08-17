a2a22e3442fff41fac5ac9769b0483ec49904cb9
shamraimaxim1307 <max.shamray234@gmail.com>
2022-08-17 17:48:25 +0300

    Edited .gitignore and added updates to kucoinseller.py and kucointelegrambot.py

---
+++ additional/kucoinseller.py
@@ -0,0 +1,159 @@
+import sys
+import time
+from os import getenv
+
+from kucoin.client import Market
+from kucoin.client import Trade
+from additional.balance.balance import get_valid_currencies
+from additional.database.models import User, Api
+import telebot
+
+bot = telebot.TeleBot("API-KEY")
+
+
+class CurrencyData:
+    def __init__(self, user_id, symbol_to_roll, symbol_income_percent, symbol_stop):
+        self.user_id = user_id
+        self.symbol_to_roll = symbol_to_roll + '-USDT'
+        self.symbol_income_percent = symbol_income_percent
+        self.symbol_stop = symbol_stop
+
+    def connect_kucoin_market(self):
+        user = User.get(User.chat_id == self.user_id)
+        api = Api.get(Api.foreign_key == user)
+        client = Market(api.api_key, api.api_secret, api.api_passphrase)
+        return client
+
+    def connect_kucoin_trade(self):
+        user = User.get(User.chat_id == self.user_id)
+        api = Api.get(Api.foreign_key == user)
+        client = Trade(api.api_key, api.api_secret, api.api_passphrase)
+        return client
+
+    def check_price(self):
+        """
+        :return: price of symbol
+        :type: str
+        """
+        client = self.connect_kucoin_market()
+        price = client.get_ticker(self.symbol_to_roll)['price']
+        return price
+
+    def sell_template(self):
+        """
+        Sell template for avoiding duplicates
+        :return: that's nothing to return, because it's template for another function
+        """
+        client = self.connect_kucoin_trade()
+        symbol_to_roll_balance = get_valid_currencies()[self.symbol_to_roll.replace('-USDT', '')]
+        symbol_price = float(self.check_price())
+        symbol_decimal = str(symbol_price)[::-1].find('.')
+        symbol_price_stop = round(symbol_price - symbol_price * self.symbol_stop, symbol_decimal)
+        symbol_price_buy = round((symbol_price + symbol_price * self.symbol_income_percent), symbol_decimal)
+        order_sell_id = client.create_limit_order(self.symbol_to_roll, 'sell', symbol_to_roll_balance,
+                                                  symbol_price_buy)['orderId']
+        bot.send_message(self.user_id,
+                         f"Stop is worked or {order_sell_id} is ordered of sale by"
+                         f" {round(symbol_price + symbol_price * self.symbol_income_percent, symbol_decimal)}")
+        print(f"Stop is worked or {order_sell_id} is ordered of sale by"
+              f" {round(symbol_price + symbol_price * self.symbol_income_percent, symbol_decimal)}")
+        self.sell_currency(order_sell_id, symbol_price_stop)
+
+    def buy_template(self):
+        """
+        :return: that's nothing to return, because it's template for another function
+        """
+        client = self.connect_kucoin_trade()
+        symbol_to_roll_balance = get_valid_currencies()[self.symbol_to_roll.replace('-USDT', '')]
+        symbol_price = float(self.check_price())
+        symbol_decimal = str(symbol_price)[::-1].find('.')
+        symbol_price_stop = round(symbol_price + symbol_price * self.symbol_stop, symbol_decimal)
+        symbol_price_sell = round((symbol_price - symbol_price * self.symbol_income_percent), symbol_decimal)
+        order_buy_id = client.create_limit_order(self.symbol_to_roll, 'buy', symbol_to_roll_balance,
+                                                 symbol_price_sell)['orderId']
+        bot.send_message(self.user_id,
+                         f"Stop is worked or {order_buy_id} is ordered of purchase by {symbol_price_sell}")
+
+        self.buy_currency(order_buy_id, symbol_price_stop)
+
+    def buy_currency(self, order_id, symbol_price_stop):
+        """
+            This function buys currency and check if stops/cancel is triggered
+            :param symbol_price_stop: symbol price for re-create order
+            :param order_id: Order ID was given by buy template
+            :return: that's function nothing to return
+            """
+        client_market = self.connect_kucoin_market()
+        client_trade = self.connect_kucoin_trade()
+        market_price = float(client_market.get_ticker(self.symbol_to_roll)['price'])
+        try:
+            while True:
+                order_buy_id = client_trade.get_order_details(order_id)
+                isOrderCancel = order_buy_id['cancelExist']
+                isOrderActive = order_buy_id['isActive']
+                if isOrderCancel is True:
+                    bot.send_message(self.user_id, '❌ALARM:ORDER IS CANCELLED❌')
+                    break
+                else:
+                    if market_price <= symbol_price_stop:
+                        bot.send_message(self.user_id, '❌ALARM:ORDER IS TRIGGERED❌')
+                        self.buy_template()
+                    if isOrderActive is False:
+                        bot.send_message(self.user_id, '✅ALARM: CURRENCY IS BOUGHT/SOLD✅')
+                        self.sell_template()
+                time.sleep(1.500)
+        except Exception as e:
+            bot.send_message(self.user_id, '❌ALARM:ERROR OCCURRED❌')
+            sys.exit(e)
+
+    def sell_currency(self, order_id, symbol_price_stop):
+        """
+        This function sells currency and check if stops/cancel is triggered
+        :param symbol_price_stop: symbol price for re-create order
+        :param order_id: Order ID was given by buy template
+        :return: that's function nothing to return
+        """
+        client_market = self.connect_kucoin_market()
+        client_trade = self.connect_kucoin_trade()
+        market_price = float(client_market.get_ticker(self.symbol_to_roll)['price'])
+        try:
+            while True:
+                order_sell_id = client_trade.get_order_details(order_id)
+                isOrderCancel = order_sell_id['cancelExist']
+                isOrderActive = order_sell_id['isActive']
+                if isOrderCancel is True:
+                    bot.send_message(self.user_id, '❌ALARM:ORDER IS CANCELLED❌')
+                    break
+                else:
+                    if market_price <= symbol_price_stop:
+                        bot.send_message(self.user_id, '❌ALARM:ORDER IS TRIGGERED❌')
+                        self.sell_template()
+                    if isOrderActive is False:
+                        bot.send_message(self.user_id, '✅ALARM: CURRENCY IS BOUGHT/SOLD✅')
+                        self.buy_template()
+                time.sleep(1.500)
+        except Exception as e:
+            bot.send_message(self.user_id, '❌ALARM:ERROR OCCURRED❌')
+            sys.exit(e)
+
+    def cancel_orders(self):
+        client = self.connect_kucoin_trade()
+        client.cancel_all_orders()
+        bot.send_message(self.user_id, 'Orders are cancelled. Rolling is stopped')
+
+    def launch_bot(self):
+        """
+        This function launch currency bot
+        :return: that's function nothing to return
+        """
+        client = self.connect_kucoin_trade()
+        client.cancel_all_orders()
+        symbol_to_roll_balance = get_valid_currencies()[self.symbol_to_roll.replace('-USDT', '')]
+        symbol_price = float(self.check_price())
+        symbol_decimal = str(symbol_price)[::-1].find('.')
+        symbol_price_sell = round(symbol_price + symbol_price * self.symbol_income_percent, symbol_decimal)
+        order_sell_id = client.create_limit_order(self.symbol_to_roll, 'sell', symbol_to_roll_balance,
+                                                  symbol_price_sell)['orderId']
+        symbol_price_stop = round(symbol_price - symbol_price * self.symbol_stop, symbol_decimal)
+        bot.send_message(self.user_id, f'Start rolling...\n{order_sell_id} is ordered for sale by {symbol_price_sell}')
+        self.sell_currency(order_sell_id, symbol_price_stop)
--- kucointelegrambot.py
+++ kucointelegrambot.py
@@ -1,5 +1,7 @@
 import logging
 
+from additional.kucoinseller import CurrencyData
+from additional.balance.balance import get_valid_currencies
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -20,6 +22,13 @@ class ApiData(StatesGroup):
     api_passphrase = State()
 
 
+class StateCurrencyData(StatesGroup):
+    user_id = ''
+    symbol_to_roll = State()
+    symbol_income_percent = State()
+    symbol_stop = State()
+
+
 @dp.message_handler(commands='start')
 async def cmd_start(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
@@ -28,24 +37,93 @@ async def cmd_start(message: types.Message):
     keyboard.add(*buttons)
     if User.select().where(User.chat_id == message.chat.id):
         await message.reply(f'Hello, {fmt.hbold(message.chat.first_name)} {fmt.hbold(message.chat.last_name)}',
-                            parse_mode=types.ParseMode.HTML
-                            , reply_markup=keyboard)
+                            parse_mode=types.ParseMode.HTML,
+                            reply_markup=keyboard)
     else:
         User.create(
             chat_id=message.chat.id,
         )
         await message.reply(f'Hello, {fmt.hbold(message.chat.first_name)} {fmt.hbold(message.chat.last_name)}',
-                            parse_mode=types.ParseMode.HTML
-                            , reply_markup=keyboard)
+                            parse_mode=types.ParseMode.HTML,
+                            reply_markup=keyboard)
+
+# TODO: Create command cancel that stop kucoin bot. Coming soon!
+# @dp.message_handler(commands='cancel')
+# async def cmd_cancel(message: types.Message):
+#     instance = CurrencyData(StateCurrencyData.user_id, StateCurrencyData.symbol_to_roll,
+#                             StateCurrencyData.symbol_income_percent, StateCurrencyData.symbol_stop)
+#     instance.cancel_orders()
+#     await message.reply('Cancelled by command!')
 
 
 @dp.message_handler(lambda message: message.text == '📈 | Start rolling currency')
 async def bot_answer(message: types.Message):
     user = User.get(User.chat_id == message.chat.id)
     if Api.select().where(Api.foreign_key == user):
-        await message.answer('ok')
+        valid_currencies = get_valid_currencies()
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        list_currencies = []
+        for key in valid_currencies.keys():
+            list_currencies.append(key)
+        keyboard.add(*list_currencies)
+        await StateCurrencyData.symbol_to_roll.set()
+
+        await message.reply('If you want cancel action enter "/cancel"\n'
+                            'Choose valid currency: ', reply_markup=keyboard)
     else:
         await message.reply('ERROR: API not set. Choose "Set API Data" button')
+
+
+@dp.message_handler(state=StateCurrencyData.symbol_to_roll)
+async def process_symbol_to_roll_key(message: types.Message, state: FSMContext):
+    keyboard = types.ReplyKeyboardRemove()
+    async with state.proxy() as data:
+        data['symbol_to_roll'] = message.text
+    await StateCurrencyData.next()
+
+    await message.reply('If you want cancel action enter "/cancel"\n'
+                        'Enter incoming percent of price you want to earn:', reply_markup=keyboard)
+
+
+@dp.message_handler(state=StateCurrencyData.symbol_income_percent)
+async def process_symbol_income_percent(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['symbol_income_percent'] = message.text
+    if data['symbol_income_percent'].isdigit():
+        await StateCurrencyData.next()
+
+        await message.reply('If you want cancel action enter "/cancel"\n'
+                            'Enter stop-order percent of price you want to stop order, if it is triggered:')
+    else:
+        await StateCurrencyData.symbol_income_percent.set()
+
+        await message.reply('ERROR: Invalid incoming percent\n'
+                            'Enter incoming percent of price you want to earn:')
+
+
+async def start_rolling():
+    instance = CurrencyData(StateCurrencyData.user_id, StateCurrencyData.symbol_to_roll,
+                            StateCurrencyData.symbol_income_percent, StateCurrencyData.symbol_stop)
+    instance.launch_bot()
+
+
+@dp.message_handler(state=StateCurrencyData.symbol_stop)
+async def process_symbol_income_percent(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['symbol_stop'] = message.text
+    if data['symbol_stop'].isdigit():
+        StateCurrencyData.user_id = message.chat.id
+        StateCurrencyData.symbol_to_roll = data['symbol_to_roll']
+        StateCurrencyData.symbol_income_percent = float(data['symbol_income_percent']) / 100
+        StateCurrencyData.symbol_stop = float(data['symbol_stop']) / 100
+        await state.finish()
+        await message.answer('Start rolling!')
+        await start_rolling()
+    else:
+        await StateCurrencyData.symbol_stop.set()
+
+        await message.reply('ERROR: Invalid stop-order percent\n'
+                            'Enter stop-order percent of price you want to stop order, if it is triggered:')
 
 
 @dp.message_handler(lambda message: message.text == '🔑 | Set API Data')
@@ -64,6 +142,7 @@ async def bot_answer(message: types.Message):
 async def cancel_handler(message: types.Message, state: FSMContext):
     current_state = await state.set_state()
     if current_state is None:
+        await message.reply('Cancelled!')
         return
     logging.info(f'Cancelling state {current_state}')
     await message.reply('Cancelled!')
@@ -105,6 +184,7 @@ async def process_api_passphrase(message: types.Message, state: FSMContext):
             api_passphrase=data['api_passphrase'],
             foreign_key=user
         )
+        await message.answer('Data is created!')
     await state.finish()
 
 
